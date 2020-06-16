ead34e2984eaae478ef17e27f42c38fc9521389f
Luiz Felipe Santos <luiz.felipe@redfox.tech>
2020-06-16 19:44:26 -0300

    pessoa equipe file added

--- src/com/teamfinder/controllers/PessoaEquipeController.java
+++ src/com/teamfinder/controllers/PessoaEquipeController.java
@@ -1,5 +1,25 @@
 package com.teamfinder.controllers;
 
+import com.teamfinder.beans.PessoaEquipeBean;
+import com.teamfinder.beans.PessoaBean;
+import com.teamfinder.beans.EquipeBean;
+import com.teamfinder.dbs.EventoEquipeDao;
+import com.teamfinder.dbs.PessoaEquipeDao;
+
+import java.sql.SQLException;
+
 public class PessoaEquipeController {
-    
+    private PessoaEquipeDao pessoaEquipeDao;
+    private PessoaController pessoaController;
+    private EquipeController equipeController;
+
+    public void inserir(PessoaEquipeBean pessoaEquipe) throws SQLException, ClassNotFoundException{
+        pessoaEquipeDao = new PessoaEquipeDao();
+        pessoaEquipeDao.inserir(pessoaEquipe);
+    }
+
+    public void remover(PessoaEquipeBean pessoaEquipe) throws SQLException, ClassNotFoundException{
+        pessoaEquipeDao = new PessoaEquipeDao();
+        pessoaEquipeDao.remover(pessoaEquipe);
+    }
 }
